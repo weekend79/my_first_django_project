410cc067ebed3a318a605656caa87843a90932d7
weekend79 <mortenviken@yahoo.com>
2020-05-10 23:25:05 +0000

    registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
     """Return the index.html file """
     return render(request, 'index.html')
 
 
+@login_required
 def logout(request):
     """Log out user"""
     auth.logout(request)
@@ -16,6 +18,8 @@ def logout(request):
 
 def login(request):
     """Return the Login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -27,9 +31,16 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect!")
-                    
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+
+
+def registration(request):
+    """Render the registration page"""
+    registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
