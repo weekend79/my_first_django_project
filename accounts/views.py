4169e0a0b9bc3e57e7153c5dffc56d5aa5722c60
weekend79 <mortenviken@yahoo.com>
2020-05-12 00:07:35 +0000

    added url-reset, accounts/urls.py, profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
@@ -40,7 +41,34 @@ def login(request):
 
 
 def registration(request):
-    """Render the registration page"""
-    registration_form = UserRegistrationForm()
+    """Render the registration page"""  
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time!")
+
+    else:
+        registration_form = UserRegistrationForm()
+
     return render(request, 'registration.html', {
         "registration_form": registration_form})
+
+
+def user_profile(request):
+    """The user's profile page"""
+    user = User.objects.get(email=request.user.email)
+    return render(request, 'profile.html', {"profile": user})
